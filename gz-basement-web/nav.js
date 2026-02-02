2bc9bed916cc5d998e7450e4607e2d89020302de
AustinSchlutter <austinschlutter@gmail.com>
2026-02-02 16:19:45 -0500

    inital

---
+++ gz-basement-web/nav.js
@@ -0,0 +1,6 @@
+// USE TO TRANFER NAV LINKS TO DIFF PAGES
+document.write('<a href="index.html" class="navlink"><b>home </b></a>')
+document.write('<a href="about.html" class="navlink">about </a>')
+document.write('<a href="events.html" class="navlink"><b>events </b></a>')
+document.write('<a href="photos.html" class="navlink"> photos </a>')
+document.write('<a href="people.html" class="navlink"><b>people</b></a>')
